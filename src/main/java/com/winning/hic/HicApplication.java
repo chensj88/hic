ac16160c62daa336df01fc04b813a9084892c1a1
chensj881008 <chenshijie1988@yeah.net>
2018-07-17 15:12:46 +0800

    资源文件添加

--- src/main/java/com/winning/hic/HicApplication.java
+++ src/main/java/com/winning/hic/HicApplication.java
@@ -1,9 +1,11 @@
 package com.winning.hic;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.winning.hic.model")
 public class HicApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/winning/hic/handler/GlobalExceptionHandler.java
@@ -0,0 +1,31 @@
+package com.winning.hic.handler;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description: 统一异常处理
+ * User: LENOVO
+ * Date: 2018-07-17
+ * Time: 12:27
+ */
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    public static final String DEFAULT_ERROR_VIEW = "error";
+
+    @ExceptionHandler(value = Exception.class)
+    public ModelAndView defaultErrorHandler(HttpServletRequest req, Exception e) throws Exception {
+        ModelAndView mav = new ModelAndView();
+        mav.addObject("exception", e);
+        mav.addObject("url", req.getRequestURL());
+        mav.setViewName(DEFAULT_ERROR_VIEW);
+        return mav;
+    }
+
+}
