a6fab881889f2494f252efebf854fcb194bbae43
Evol <798615946@qq.com>
2018-07-17 14:30:48 +0800

    spring boot 框架搭建

---
+++ src/main/java/com/winning/hic/HicApplication.java
@@ -0,0 +1,12 @@
+package com.winning.hic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HicApplication.class, args);
+	}
+}
