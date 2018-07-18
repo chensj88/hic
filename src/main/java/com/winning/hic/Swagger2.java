152430c8c1606cddc7201092f21ca9b56979b805
chensj881008 <chenshijie1988@yeah.net>
2018-07-18 09:16:17 +0800

    Swagger2增加使用说明

--- src/main/java/com/winning/hic/Swagger2.java
+++ src/main/java/com/winning/hic/Swagger2.java
@@ -16,6 +16,9 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
  * 它可以轻松的整合到Spring Boot中，并与Spring MVC程序配合组织出强大RESTful API文档。
  * 它既可以减少我们创建文档的工作量，同时说明内容又整合入实现代码中，
  * 让维护文档和修改代码整合为一体，可以让我们在修改代码逻辑的同时方便的修改文档说明
+ * @ApiOperation注解来给API增加说明、
+ * @ApiImplicitParams、@ApiImplicitParam注解来给参数增加说明
+ * 访问路径：http://host:port/project/swagger-ui.html
  * User: LENOVO
  * Date: 2018-07-17
  * Time: 12:14
@@ -37,8 +40,7 @@ public class Swagger2 {
     private ApiInfo apiInfo() {
         return new ApiInfoBuilder()
                 .title("Spring Boot中使用Swagger2构建RESTful APIs")
-                .description("更多Spring Boot相关文章请关注：http://blog.didispace.com/")
-                .termsOfServiceUrl("http://blog.didispace.com/")
+                .description("使用Swagger2构建项目RESTful APIs")
                 .contact("SSGJ")
                 .version("1.0")
                 .build();
--- src/main/java/com/winning/hic/web/PageController.java
+++ src/main/java/com/winning/hic/web/PageController.java
@@ -1,8 +1,12 @@
 package com.winning.hic.web;
 
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  * Created with IntelliJ IDEA.
@@ -14,8 +18,29 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class PageController {
 
-    @RequestMapping(value = "/")
+    @ApiOperation(value = "首页信息",notes = "")
+    @RequestMapping(value = "/",method = RequestMethod.GET)
     public String login(){
         return "login/login";
+    }
+
+    @ApiOperation(value = "验证用户是否存在" ,notes="验证输入的用户ID是否存在")
+    @ApiImplicitParam(name = "userId", value = "用户ID", required = true, dataType = "String")
+    @RequestMapping(value = "/checkHaveUser",method = RequestMethod.POST)
+    public String checkHaveUser(String userId){
+        System.out.println(userId);
+        return "success";
+    }
+
+    @ApiOperation(value = "登录信息验证" ,notes="验证输入的用户和密码")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "userId", value = "用户ID", required = true, dataType = "String"),
+            @ApiImplicitParam(name = "password", value = "用户密码", required = true, dataType = "String")
+    })
+    @RequestMapping(value = "/login",method = RequestMethod.POST)
+    public String checkLogin(String userId,String password){
+        System.out.println(userId);
+        System.out.println(password);
+        return "success";
     }
 }
