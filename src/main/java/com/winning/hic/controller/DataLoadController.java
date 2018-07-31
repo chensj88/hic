b62862f576f0f348bf067a1708cbc7aba5e97296
chenkuai2017 <827995732@qq.com>
2018-07-31 13:19:30 +0800

    陈蒯 数据抽取

--- src/main/java/com/winning/hic/controller/DataLoadController.java
+++ src/main/java/com/winning/hic/controller/DataLoadController.java
@@ -1,7 +1,10 @@
 package com.winning.hic.controller;
 
+import com.winning.hic.model.EmrQtbljlk;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 public class DataLoadController extends BaseController{
@@ -9,12 +12,18 @@ public class DataLoadController extends BaseController{
     @RequestMapping("/dataLoad/index")
     public String index() {
         //数据抽取
+        EmrQtbljlk qtbljlk = new EmrQtbljlk();
+        List<EmrQtbljlk> qtbljlkList = super.getFacade().getEmrQtbljlkService().getEmrQtbljlkList(qtbljlk);
+        String xml = qtbljlkList.get(0).getBlnr();
 
 
 
 
         return "/dataLoad/index";
     }
+
+
+
 
 
 
