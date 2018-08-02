64dd81189e1848a507bc5a9c57575750a3f2bc16
chenkuai2017 <827995732@qq.com>
2018-08-02 10:21:11 +0800

    陈蒯 数据抽取

--- src/main/java/com/winning/hic/controller/DataLoadController.java
+++ src/main/java/com/winning/hic/controller/DataLoadController.java
@@ -1,8 +1,11 @@
 package com.winning.hic.controller;
 
+import com.winning.hic.base.utils.XmlUtil;
 import com.winning.hic.model.EmrQtbljlk;
+import com.winning.hic.model.HlhtZybcjlScbcjl;
 import org.dom4j.Attribute;
 import org.dom4j.Document;
+import org.dom4j.DocumentHelper;
 import org.dom4j.Element;
 import org.dom4j.io.SAXReader;
 import org.springframework.stereotype.Controller;
@@ -20,24 +23,23 @@ public class DataLoadController extends BaseController{
         try{
             EmrQtbljlk qtbljlk = new EmrQtbljlk();
             List<EmrQtbljlk> qtbljlkList = super.getFacade().getEmrQtbljlkService().getEmrQtbljlkList(qtbljlk);
-            String xml = qtbljlkList.get(0).getBlnr();
-            SAXReader reader = new SAXReader();
-            Document document = reader.read(xml);
-            // 通过document对象获取根节点node
-            Element nodeStore = document.getRootElement();
-            Iterator it =nodeStore.elementIterator();
-            while(it.hasNext()){
-                Element node = (Element) it.next();
-                // 获取book的属性名以及 属性值
-                List<Attribute> nodeAttrs = node.attributes();
-                for(Attribute node_son:nodeAttrs){
-                    System.out.println(node_son.getName()+"wei:"+node_son.getQName()+"值域："+node_son.getValue()+"为止:"+node_son.getNamespace());
-                }
+            StringBuffer xml= new StringBuffer();
+            xml.append(new String(qtbljlkList.get(0).getBlnr().getBytes("ASCII"),"UTF-8"));
 
-            }
+            //xml文件解析
+            Document doc =null;
+            doc = DocumentHelper.parseText(xml.toString()); // 将字符串转为XML
+            Element rootElt = doc.getRootElement();
+            System.out.println("根节点：" + rootElt.getName()); // 拿到根节点的名称
+            Iterator iter_ref = rootElt.elementIterator("Ref");
+            Iterator iter_node = rootElt.elementIterator("node");
+            List<Element> elements = XmlUtil.getNodes(rootElt);
 
-
-
+            HlhtZybcjlScbcjl entity = new HlhtZybcjlScbcjl(); //首次病程记录表
+            entity.setYjlxh("QTBLJLXH");
+            entity.setJzlsh("SYXH");
+            entity.setPatid("");
+            entity.setBch("1111");
 
         }catch (Exception e){
             e.printStackTrace();
