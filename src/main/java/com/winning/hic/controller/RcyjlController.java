4c100171d73f5bd7bf3776f91feb86fbb77581b1
chensj881008 <chenshijie1988@yeah.net>
2018-08-06 13:14:25 +0800

    1、完善代码功能

---
+++ src/main/java/com/winning/hic/controller/RcyjlController.java
@@ -0,0 +1,16 @@
+package com.winning.hic.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: LENOVO
+ * Date: 2018-08-06
+ * Time: 11:12
+ */
+@RestController
+public class RcyjlController {
+
+
+}
