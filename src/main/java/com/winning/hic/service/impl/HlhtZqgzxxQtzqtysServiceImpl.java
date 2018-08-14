35a728ec099af03a12016ee3e45d2b86b66b31c7
chenkuai2017 <827995732@qq.com>
2018-08-14 14:48:27 +0800

    陈蒯 数据抽取

--- src/main/java/com/winning/hic/service/impl/HlhtZqgzxxQtzqtysServiceImpl.java
+++ src/main/java/com/winning/hic/service/impl/HlhtZqgzxxQtzqtysServiceImpl.java
@@ -1,12 +1,23 @@
 package com.winning.hic.service.impl;
 
+import com.winning.hic.base.Constants;
+import com.winning.hic.base.utils.Base64Utils;
+import com.winning.hic.base.utils.HicHelper;
+import com.winning.hic.base.utils.ReflectUtil;
+import com.winning.hic.base.utils.XmlUtil;
+import com.winning.hic.dao.cisdb.EmrQtbljlkDao;
 import com.winning.hic.dao.data.HlhtZqgzxxQtzqtysDao;
-import com.winning.hic.model.HlhtZqgzxxQtzqtys;
+import com.winning.hic.dao.data.MbzDataListSetDao;
+import com.winning.hic.model.*;
 import com.winning.hic.service.HlhtZqgzxxQtzqtysService;
+import com.winning.hic.service.MbzDataSetService;
+import org.dom4j.Document;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.text.ParseException;
 import java.util.List;
+import java.util.Map;
 
 
 /**
@@ -21,6 +32,16 @@ public class HlhtZqgzxxQtzqtysServiceImpl implements  HlhtZqgzxxQtzqtysService {
 
     @Autowired
     private HlhtZqgzxxQtzqtysDao hlhtZqgzxxQtzqtysDao;
+
+    @Autowired
+    private MbzDataSetService mbzDataSetService;
+
+    @Autowired
+    private MbzDataListSetDao mbzDataListSetDao;
+
+    @Autowired
+    private EmrQtbljlkDao emrQtbljlkDao;
+
 
     public int createHlhtZqgzxxQtzqtys(HlhtZqgzxxQtzqtys hlhtZqgzxxQtzqtys){
         return this.hlhtZqgzxxQtzqtysDao.insertHlhtZqgzxxQtzqtys(hlhtZqgzxxQtzqtys);
@@ -49,4 +70,71 @@ public class HlhtZqgzxxQtzqtysServiceImpl implements  HlhtZqgzxxQtzqtysService {
     public List<HlhtZqgzxxQtzqtys> getHlhtZqgzxxQtzqtysPageList(HlhtZqgzxxQtzqtys hlhtZqgzxxQtzqtys){
         return this.hlhtZqgzxxQtzqtysDao.selectHlhtZqgzxxQtzqtysPageList(hlhtZqgzxxQtzqtys);
     }
+
+    private HlhtZqgzxxQtzqtys getInitialHlhtZqgzxxQtzqtys(HlhtZqgzxxQtzqtys hlhtZqgzxxQtzqtys) {
+        return this.hlhtZqgzxxQtzqtysDao.selectInitialHlhtZqgzxxQtzqtys(hlhtZqgzxxQtzqtys);
+    }
+
+    @Override
+    public List<MbzDataCheck> interfaceHlhtZqgzxxQtzqtys() {
+        //执行过程信息记录
+        List<MbzDataCheck> mbzDataChecks = null;
+
+        MbzDataSet mbzDataSet = new MbzDataSet();
+        mbzDataSet.setSourceType(Constants.WN_ZQGZXX_QTZQTYS_SOURCE_TYPE);
+        mbzDataSet.setPId(Long.parseLong(Constants.WN_ZQGZXX_QTZQTYS_SOURCE_TYPE));
+        List<MbzDataSet> mbzDataSetList = mbzDataSetService.getMbzDataSetList(mbzDataSet);
+        //1.获取对应的首次病程的模板ID集合
+        MbzDataListSet mbzDataListSet = new MbzDataListSet();
+        mbzDataListSet.setSourceType(Constants.WN_ZQGZXX_QTZQTYS_SOURCE_TYPE);
+        List<MbzDataListSet> dataListSets = this.mbzDataListSetDao.selectMbzDataListSetList(mbzDataListSet);
+        try{
+            //获取首次病程的对象集合
+            Map<String, String> paramTypeMap = ReflectUtil.getParamTypeMap(HlhtZqgzxxQtzqtys.class);
+            for(MbzDataListSet dataListSet :dataListSets){
+                //2.根据首次病程去找到对应的病人病历
+                EmrQtbljlk qtbljlk = new EmrQtbljlk();
+                qtbljlk.setBldm(dataListSet.getModelCode());
+                List<EmrQtbljlk> qtbljlkList = emrQtbljlkDao.selectEmrQtbljlkList(qtbljlk);
+
+                if(qtbljlkList != null){
+                    for(EmrQtbljlk emrQtbljlk:qtbljlkList){
+                        HlhtZqgzxxQtzqtys scbcjl = new HlhtZqgzxxQtzqtys();
+                        scbcjl.setYjlxh(String.valueOf(emrQtbljlk.getQtbljlxh()));
+                        scbcjl = this.getHlhtZqgzxxQtzqtys(scbcjl);
+
+                        if(scbcjl != null ){
+                            //初始化数据
+                            HlhtZqgzxxQtzqtys oldRcyjl  = new HlhtZqgzxxQtzqtys();
+                            oldRcyjl.setYjlxh(String.valueOf(emrQtbljlk.getQtbljlxh()));
+                            this.removeHlhtZqgzxxQtzqtys(oldRcyjl);
+                        }
+                        HlhtZqgzxxQtzqtys entity = new HlhtZqgzxxQtzqtys();
+                        entity.getMap().put("QTBLJLXH",emrQtbljlk.getQtbljlxh());
+                        entity = this.getInitialHlhtZqgzxxQtzqtys(entity);
+                        Document document = XmlUtil.getDocument(Base64Utils.unzipEmrXml(emrQtbljlk.getBlnr()));
+                        try {
+                            entity = (HlhtZqgzxxQtzqtys) HicHelper.initModelValue(mbzDataSetList, document, entity, paramTypeMap);
+                        } catch (ParseException e) {
+                            e.printStackTrace();
+                        }
+                        this.createHlhtZqgzxxQtzqtys(entity);
+
+                    }
+                }
+
+            }
+
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+
+
+
+        return null;
+    }
+
+
 }
