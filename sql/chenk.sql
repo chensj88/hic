459ce42e4dd731f45d3a72bae8fc38bef1009fcf
chenkuai2017 <827995732@qq.com>
2018-08-30 17:24:08 +0800

    陈蒯 数据抽取

--- sql/chenk.sql
+++ sql/chenk.sql
@@ -15,3 +15,29 @@ UPDATE CISDB_DATA..HLHT_ZYBCJL_SCBCJL SET jzzyzhbm=NULL WHERE jzzyzhbm='N'
 UPDATE CISDB_DATA..HLHT_ZYBCJL_SCBCJL SET jzzyzhmc=NULL WHERE jzzyzhmc='N'
 UPDATE CISDB_DATA..HLHT_ZYBCJL_SCBCJL SET zfbm='无' WHERE zfbm='N'
 UPDATE CISDB_DATA..HLHT_ZYBCJL_SCBCJL SET zzzf='无' WHERE zzzf='N'
+
+--抢救记录
+
+UPDATE A SET A.jbzdmc = C.ZDMC,A.jbzdbm = C.ZDDM
+FROM CISDB_DATA..HLHT_ZYBCJL_QJJL A LEFT JOIN CISDB.dbo.EMR_BRSYK B ON A.jzlsh =B.HISSYXH
+  LEFT JOIN CISDB.dbo.EMR_BRZDQK C ON B.SYXH = C.SYXH AND C.ZDLB = 1
+WHERE  ( CONVERT(varchar,A.jbzdmc) ='N' OR CONVERT(varchar,A.jbzdbm) ='N' );
+
+UPDATE CISDB_DATA..HLHT_ZYBCJL_QJJL SET jcjyxmmc='无' WHERE CONVERT(varchar,jcjyxmmc) ='N'
+UPDATE CISDB_DATA..HLHT_ZYBCJL_QJJL SET jrwdm=NULL
+UPDATE CISDB_DATA..HLHT_ZYBCJL_QJJL SET jcjyjg='无' WHERE CONVERT(varchar,jcjyjg) ='N'
+UPDATE CISDB_DATA..HLHT_ZYBCJL_QJJL SET jcjydljg=NULL WHERE jcjydljg ='-9.0000'
+UPDATE CISDB_DATA..HLHT_ZYBCJL_QJJL SET jcjyjgdm='无' WHERE CONVERT(varchar,jcjyjgdm) ='N'
+UPDATE CISDB_DATA..HLHT_ZYBCJL_QJJL SET zysx='无' WHERE CONVERT(varchar,zysx) ='N'
+
+UPDATE 	A SET A.qmrq=B.FSSJ from  CISDB_DATA..HLHT_ZYBCJL_QJJL A
+LEFT JOIN CISDB.dbo.EMR_QTBLJLK B ON A.yjlxh =B.QTBLJLXH
+WHERE A.qmrq='1990-01-01 00:00:00.000' ;
+UPDATE D SET D.zyzwlbdm = F.dm ,D.zyzwlbmc = F.mc   FROM CISDB_DATA.dbo.HLHT_ZYBCJL_QJJL D LEFT JOIN (
+   SELECT  stuff((select ',' + A.ZCDM from CISDB.dbo.SYS_ZGDMK A where A.ID in (
+select * from f_splitSTR(C.cjqjrydm,',')) for xml path('')),1,1,'') as dm,
+           stuff((select ',' + B.ZCMC from CISDB.dbo.SYS_ZGDMK B where B.ID in (
+select * from f_splitSTR(C.cjqjrydm,',')) for xml path('')),1,1,'')  as mc,
+     C.yjlxh
+   from CISDB_DATA.dbo.HLHT_ZYBCJL_QJJL C ) F ON D.yjlxh = F.yjlxh where (D.zyzwlbdm = 'N' OR D.zyzwlbmc='N')
+
