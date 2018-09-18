fa125bb0c794d759508b118021bfca4815e48d0f
chensj.dev <chenshijie1988@yeah.net>
2018-09-18 10:35:15 +0800

    增加门诊和住院存储

--- src/main/java/com/winning/hic/service/impl/HlhtZybcjlScbcjlServiceImpl.java
+++ src/main/java/com/winning/hic/service/impl/HlhtZybcjlScbcjlServiceImpl.java
@@ -348,6 +348,7 @@ public class HlhtZybcjlScbcjlServiceImpl implements  HlhtZybcjlScbcjlService {
 
     @Override
     public void selectAllHandleQuery() {
-        this.hlhtZybcjlScbcjlDao.selectAllHandleQuery();
+        this.hlhtZybcjlScbcjlDao.selectAllHandleQueryMZ();
+        this.hlhtZybcjlScbcjlDao.selectAllHandleQueryZY();
     }
 }
