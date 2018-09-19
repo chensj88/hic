e74610e3ebe900bfb71ba7adc5f039786ae5e686
Evol <798615946@qq.com>
2018-09-19 18:11:48 +0800

    链接服务器sql更新

--- run/环境部署/01-sql/01-链接服务器.sql
+++ run/环境部署/01-sql/01-链接服务器.sql
@@ -3,4 +3,10 @@ exec sp_addlinkedserver 'HLHT_ZY_HIS','','SQLOLEDB','129.9.1.68'
 exec sp_addlinkedsrvlogin 'HLHT_ZY_HIS','false',null,'ssa','20031118' 
 --HIS链接服务器服务器 门诊
 exec sp_addlinkedserver 'HLHT_MZ_HIS','','SQLOLEDB','129.9.1.68'  
-exec sp_addlinkedsrvlogin 'HLHT_MZ_HIS','false',null,'ssa','20031118' 
+exec sp_addlinkedsrvlogin 'HLHT_MZ_HIS','false',null,'ssa','20031118'
+--HIS链接服务器服务器 住院
+exec sp_addlinkedserver 'HLHT_ZY_HIS','','SQLOLEDB','172.16.0.200\ssgj'
+exec sp_addlinkedsrvlogin 'HLHT_ZY_HIS','false',null,'sa','zyc@8468'
+--HIS链接服务器服务器 门诊
+exec sp_addlinkedserver 'HLHT_MZ_HIS','','SQLOLEDB','172.16.0.200\ssgj'
+exec sp_addlinkedsrvlogin 'HLHT_MZ_HIS','false',null,'sa','zyc@8468'
