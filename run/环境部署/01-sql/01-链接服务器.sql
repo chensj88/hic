f0d846103c6c43f73eaf61a9dd7078fbcd24febd
chensj.dev <chenshijie1988@yeah.net>
2018-09-21 14:15:21 +0800

    增加本地配置 增加内蒙古数据

--- run/环境部署/01-sql/01-链接服务器.sql
+++ run/环境部署/01-sql/01-链接服务器.sql
@@ -15,7 +15,7 @@ exec sp_addlinkedsrvlogin 'HLHT_MZ_HIS','false',null,'sa','zyc@8468'
 --###############内蒙古人民医院###################################
 --HIS链接服务器服务器 住院
 exec sp_addlinkedserver 'HLHT_ZY_HIS','','SQLOLEDB','18.5.1.159\EJZ'
-exec sp_addlinkedsrvlogin 'HLHT_ZY_HIS','false',null,'sa','wwinning'
+exec sp_addlinkedsrvlogin 'HLHT_ZY_HIS','false',null,'sa','winning'
 --HIS链接服务器服务器 门诊
 exec sp_addlinkedserver 'HLHT_MZ_HIS','','SQLOLEDB','18.5.1.159\EJZ'
-exec sp_addlinkedsrvlogin 'HLHT_MZ_HIS','false',null,'sa','wwinning'
+exec sp_addlinkedsrvlogin 'HLHT_MZ_HIS','false',null,'sa','winning'
